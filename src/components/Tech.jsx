0703e8ec920c37415af6d9ba2acda0c669962e99
Sarguru02 <psgnhealth@gmail.com>
2023-04-23 19:39:31 +0530

    new complete contact section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";import { fadeIn, textVariant } from "../utils/motion";
 
 const Tech = () => {
-  return (
+  return (<>
+  <motion.div variants={textVariant()}>
+      <h1 className={styles.sectionHeadText}>My Skills</h1>
+  </motion.div>
+  <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >These are the basic Skills that I am proficient in.
+        </motion.p>
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
@@ -13,6 +22,7 @@ const Tech = () => {
         </div>
       ))}
     </div>
+    </>
   );
 };
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -81,10 +81,6 @@ const technologies = [
     icon: reactjs,
   },
   {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
     name: "Tailwind CSS",
     icon: tailwind,
   },
@@ -93,24 +89,12 @@ const technologies = [
     icon: nodejs,
   },
   {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
     name: "Three JS",
     icon: threejs,
   },
   {
     name: "git",
     icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
   },
 ];
 
